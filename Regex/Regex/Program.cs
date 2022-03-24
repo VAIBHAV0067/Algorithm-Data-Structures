ec5a0e43166cc4fb9feb41f9f0c23bd4488b6cab
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-24 16:45:45 +0530

    Problem12

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the string");
+            string pincode = Console.ReadLine();
+            Patterns patterns = new Patterns();
+            if (patterns.validatePinCode(pincode))
+                Console.WriteLine("Pattern Matched");
+            else
+                Console.WriteLine("Pattern Not Matched");
+        }
+    }
+}
