aa2fec0eaa062916f14874b3aa70af7e90949c19
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-28 19:02:08 +0530

    Program7

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structure Problems");
+            Console.WriteLine("Press 6 for Primenumber Problem");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            PrimeNumber1 pnum = new Primeumber1();
+            pnum.Prime();
+        }
+    }
+}
